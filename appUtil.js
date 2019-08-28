6f892f6df97ee84153690150891132dc64cc169e
sanjucta.ghose <sanjuctab@gmail.com>
2019-08-28 18:39:42 +0530

    Removed extraneous code

--- appUtil.js
+++ appUtil.js
@@ -1,24 +1,6 @@
 require('dotenv').config();
 var pool = require('./database');   
-const crypto = require('crypto');
 
-function validateSignature(query) {
-    var parameters = [];
-    for (var key in query) {
-      if (key != 'signature') {
-        parameters.push(key + '=' + query[key])
-      }
-    }
-    var message = parameters.sort().join('');
-    var digest = crypto.createHmac('sha256', process.env.SHOPIFY_API_SECRET_KEY).update(message).digest('hex');
-    console.log("Digest is ::");
-    console.log(digest);
-    console.log("query.signature is ::");
-    console.log(query.signature);
-
-
-    return digest === query.signature;
-  };
 
   async function getAccessTokenforDomain(shop_domain) {
     const queryResult = await pool.query(`select access_token from shop_tokens where shop_domain="${shop_domain}"`);
